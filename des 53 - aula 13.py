0636db28794fd4e9ada25da1b59257a46ffa370a
Leonardo Heckmann <114787699+leoheckmann@users.noreply.github.com>
2023-01-22 18:40:06 -0300

    Update des 53 - aula 13.py

--- des 53 - aula 13.py
+++ des 53 - aula 13.py
@@ -7,3 +7,16 @@
 # a torre da derrota
 # O lobo ama o bolo
 # anotaram a data da maratona
+
+
+frase = str(input('Digite a frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(junto, inverso)
+if inverso == junto:
+    print('Essa frase é palíndromo')
+else:
+    print('Essa frase não é palíndromo')
