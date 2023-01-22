678ea2bb261d751dde9a0a822e93ce7df674f6ae
Leonardo Heckmann <114787699+leoheckmann@users.noreply.github.com>
2023-01-22 18:41:12 -0300

    Update des 56 - aula 13.py

--- des 56 - aula 13.py
+++ des 56 - aula 13.py
@@ -6,11 +6,26 @@
 #Quantas mulheres têm menos de 20 anos.
 
 
-idade_media = 0
-
-for peso in range(1, 5):
-    nome = str(input('Insira o nome: '))
+for cent in range(1, 5):
+    print(f'-=-=-=-=-=-= {cent} -=-=-=-=-=-=')
+    nome = str(input('Insira o nome: ')).upper()
+    sexo = str(input('Seu sexo: ')).upper()
     idade = int(input('Insira a idade: '))
-    peso = float(input(f'Insira o peso: '))
-    idade_media = idade_media + idade / 4
-print(idade_media)
+    soma_idade += idade
+    if cent == 1 and sexo in 'Mm':
+        man_older = idade
+        name_older = nome
+    if sexo in 'Mm' and idade > man_older:
+        man_older = idade
+        name_older = nome
+    if sexo in 'Ff' and idade < 20:
+        woman20 += 1
+
+
+idade_media = soma_idade / 4
+
+
+print(f'A média da idade do grupo é de {idade_media} anos!')
+print(f'A idade do homem mais velho é de {man_older} anos e o nome dele é {name_older}')
+print(f'Ao todo são {woman20} mulheres que tem menos de 20 anos')
+
